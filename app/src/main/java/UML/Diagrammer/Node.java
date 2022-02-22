ae0e2836638fa8c15248804ecf3e7741b3db1156
Alex <dandiviney@isu.edu>
2022-02-21 22:41:24 -0700

    Added default node and edge interfaces and abstract classes.

---
+++ app/src/main/java/UML/Diagrammer/Node.java
@@ -0,0 +1,32 @@
+//Node.java
+package UML.Diagrammer;
+
+/**
+ * This interface should define what methods every single UML "object" (not including edges) should have.
+ * @author Alex Diviney
+ */
+public interface Node {
+
+//    /**
+//     *
+//     * @return name of object
+//     */
+//    String getName();
+//
+//    /**
+//     *
+//     * @return unique id of object
+//     */
+//    int getID(); //ID should be unique object value.
+
+//    /**
+//     * Sets the Size of the diagram. (width, height, radius etc.)
+//     */
+//    void setSize();
+
+    /**
+     * Sets the x and y position on the page of the object.
+     */
+    void setCoords(int x, int y);
+
+}
