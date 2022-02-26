97b84c860bea3e12a0d8af2d6084fc4ec7a7453e
Alex <dandiviney@isu.edu>
2022-02-26 01:12:50 -0700

    Re added App.Launch() in the desktop app main method

--- app/src/main/java/UML/Diagrammer/desktop/App.java
+++ app/src/main/java/UML/Diagrammer/desktop/App.java
@@ -20,7 +20,7 @@ public class App extends Application{
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         System.out.println(new DefaultNode().getTitle());
-
+        App.launch();
 //        Javalin app = Javalin.create().start(7070);
 //        app.get("/", ctx -> ctx.result("Hello this is a test"));
 
