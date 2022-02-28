5704ccaaba0ebbf8505dcc53bdde4daee94cf319
barlekow <barlekow@isu.edu>
2022-02-28 15:51:00 -0700

    Test Commit

---
+++ backend/src/test/java/EdgeTest.java
@@ -0,0 +1,19 @@
+
+import UML.Diagrammer.backend.objects.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EdgeTest {
+
+    @Test
+    public void setNodes() {
+    }
+
+    @Test
+    public void getID() {
+    }
+}
---
+++ backend/src/test/java/NodeTest.java
@@ -0,0 +1,26 @@
+
+import UML.Diagrammer.backend.objects.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class NodeTest {
+
+    @Test
+    public void setCoords() {
+    }
+
+    @Test
+    public void getID() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+}
