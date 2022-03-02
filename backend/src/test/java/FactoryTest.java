72239caa800a88dcac06d1897ca8a3794caa436c
pratshow <pratshow@isu.edu>
2022-03-01 21:09:37 -0700

    Small additions to the factory class. Need to think about how specificially how we want to set up this class

--- backend/src/test/java/FactoryTest.java
+++ backend/src/test/java/FactoryTest.java
@@ -1,10 +1,15 @@
 import UML.Diagrammer.backend.objects.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class FactoryTest {
+
+    Factory factory;
+    @BeforeEach
+    public void makeFactory(){
+        factory = new Factory();
+    }
+
     @Test
     public void testFactory(){
         Factory factory = new Factory();
@@ -20,6 +25,28 @@ public class FactoryTest {
         DefaultNode node = factory.createSmallDefaultNode("test",0,0);
         assertNotNull(node.getHeight());
         assertNotNull(node.getWidth());
+    }
+
+    @Test
+    public void testGenericNodeWithName(){
+        DefaultNode node = factory.createGenericNode("customName", 0, 0);
+        assertEquals("customName", node.getName());
+        assertEquals("input a description", node.getDescription());
+        assertEquals(0, node.getXCoord());
+        assertEquals(0, node.getYCoord());
+        assertEquals(2, node.getWidth());
+        assertEquals(2, node.getHeight());
+    }
+
+    @Test
+    public void testGenericNodeWithNoName(){
+        DefaultNode node = factory.createGenericNode(0, 0);
+        assertEquals("New object", node.getName());
+        assertEquals("input a description", node.getDescription());
+        assertEquals(0, node.getXCoord());
+        assertEquals(0, node.getYCoord());
+        assertEquals(2, node.getWidth());
+        assertEquals(2, node.getHeight());
     }
 
 }
