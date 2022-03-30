2c8eb9104e91a33cabe42c29345e577bdd41f40b
barlekow <barlekow@isu.edu>
2022-03-29 18:50:29 -0600

    HTTPClient Commit

---
+++ backend/src/test/java/HTTP_ClientTest.java
@@ -0,0 +1,48 @@
+import UML.Diagrammer.backend.objects.*;
+import UML.Diagrammer.backend.objects.NodeFactory.*;
+import UML.Diagrammer.backend.objects.HTTP_Client.*;
+import org.javalite.activejdbc.Base;
+import org.javalite.activejdbc.connection_config.DBConfiguration;
+import org.javalite.activejdbc.test.DBSpec;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assertions.*;
+
+public class HTTP_ClientTest {
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void exampleGetRequest() {
+        HTTP_Client client = new HTTP_Client();
+
+    }
+
+    @Test
+    public void examplePutRequest() {
+    }
+
+    @Test
+    public void examplePostRequest() {
+    }
+
+    @Test
+    public void tryLoginUser() {
+    }
+
+    @Test
+    public void tryGetPage() {
+    }
+
+    @Test
+    public void getUserpageNames() {
+    }
+
+    @Test
+    public void sendCurrentPageState() {
+    }
+}
