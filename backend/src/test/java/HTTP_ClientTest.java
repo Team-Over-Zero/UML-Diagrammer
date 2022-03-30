66d67711eeb6f1f9056c70d396ed8364406c23ab
barlekow <barlekow@isu.edu>
2022-03-29 19:11:14 -0600

    First Test Commits

--- backend/src/test/java/HTTP_ClientTest.java
+++ backend/src/test/java/HTTP_ClientTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HTTP_ClientTest {
 
