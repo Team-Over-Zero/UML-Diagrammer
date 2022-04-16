3791c401c86d7c78523938c5e106126385629fed
TotallyJuice <michaelcollier10@gmail.com>
2022-04-16 14:24:45 -0600

    can load nodes from json over server

--- webserver/src/main/resources/vue/views/uml-editor.vue
+++ webserver/src/main/resources/vue/views/uml-editor.vue
@@ -5,7 +5,7 @@
     </h1>
     <div>
       <button v-on:click="vparseJsonFromDiagram()">Save</button>
-      <button>Load</button>
+      <button v-on:click="vloadNode()">Load</button>
       <button>Export</button>
       <select name="left-arrow" id="left-arrow">
         <option value="<--"><--</option>
@@ -49,6 +49,18 @@
 
   let canvas = new fabric.Canvas('c');
   let nodes = [];
+
+  let defaultNode = {
+    description: "A description",
+    height: 50,
+    width: 50,
+    x_coord: 10,
+    y_coord: 10,
+    name: "name",
+    svg_image: "",
+    type: "",
+    id: -1,
+  };
 
   function registerCanvas(){
     canvas = new fabric.Canvas('c');
@@ -123,37 +135,72 @@
 
   }
 
-  function addNode(path, svg_image, type){
+  /*function loadNode(jsonString){
+    //let n = JSON.parse(jsonString);
+  let n=jsonString;
+    fetch('/svg/' + n.svg_image)
+        .then(result => result.text())
+        .then((output) => {
+          addNode(output, n.svg_image, n.type, n.name, n.x_coord, n.y_coord);
+        }).catch(err => console.error(err));
+
+    addNode()
+
+
+  }*/
+
+
+
+
+  function addNode(path, svg_image, type, name = "name", xCoord = 10, yCoord = 10, id = -1){
 
     let shape = new fabric.Path(path, {
       left: 0,
       top: 0
     });
 
-    let nameText = new fabric.Textbox('hello', {
+    let nameText = new fabric.Textbox(name, {
       left: shape.getScaledWidth() * 0.05,
       top: shape.getScaledHeight() * 0.1,
       width: shape.getScaledWidth() * 0.9,
+      fontSize: 12,
     });
 
     let svgText = new fabric.Textbox(svg_image, {
+      width: 0,
       fill: 'rgb(0,0,0,0)'
     });
 
     let typeText = new fabric.Textbox(type, {
+      width: 0,
+      fill: 'rgb(0,0,0,0)'
+    });
+
+    let idText = new fabric.Textbox(id.toString(), {
+      width: 0,
       fill: 'rgb(0,0,0,0)'
     });
 
 
-    let group = new fabric.Group([shape, nameText, svgText, typeText], {
-      top: 10,
-      left: 10
+    let group = new fabric.Group([shape, nameText, svgText, typeText, idText], {
+      top: yCoord,
+      left: xCoord
     });
 
-
+    group.setControlsVisibility({
+      mt: false, // middle top
+      mb: false, // midle bottom
+      ml: false, // middle left
+      mr: false, // middle right
+      mtr: false // rotate
+    });
 
     canvas.add(group);
 
+  }
+
+  function removeCurrentNode(){
+    canvas.remove(canvas.getActiveObject());
   }
 
   function parseJsonFromDiagram(){
@@ -169,7 +216,7 @@
         name: object._objects[1].text,
         svg_image: object._objects[2].text,
         type: object._objects[3].text,
-        id: -1,
+        id: parseInt(object._objects[4].text),
       };
       json = JSON.stringify(n)
       console.log(json);
@@ -218,7 +265,21 @@
             }).catch(err => console.error(err));
       },
 
-      loadSVG: function(){
+      vloadNode: function(){
+        //let n = {description:"Default Description",height:152,width:152,x_coord:363.82059800664456,y_coord:191.78807947019868,name:"A very good text",svg_image:"TextBox_Square_Interface.svg",type:"text_box_nodes",id:-1};
+
+        fetch('/node/1')
+            .then(result => result.text())
+            .then((output) => {
+              let n = JSON.parse(output);
+              console.log(n);
+              fetch('/svg/' + n.svg_image)
+                  .then(result => result.text())
+                  .then((output) => {
+                    console.log(output);
+                    addNode(output, n.svg_image, n.type, n.name, n.x_coord, n.y_coord);
+                  }).catch(err => console.error(err));
+            }).catch(err => console.error(err));
 
       },
 
@@ -232,6 +293,10 @@
   });
 
 
+
+  document.addEventListener('keyup', (e) => {
+    if (e.code === "Delete")        removeCurrentNode();
+  });
 
 </script>
 
